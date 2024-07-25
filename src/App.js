eb61c25f099cd7ef6382ab88fb82d84047a986cb
p0k0rwio <you@example.com>
2024-07-25 12:20:50 +0530

    added layout and installed packages via npm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/jquery/dist/jquery.js";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/Home/Home.js";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
