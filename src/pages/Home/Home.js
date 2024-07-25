7627e65ed6aedbfb7c375906ce00f3aded0a1473
p0k0rwio <you@example.com>
2024-07-25 15:55:22 +0530

    changes in dashboard

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import MainLayout from '../../components/Layouts/MainLayout/MainLayout'
-import "./Home.css"
+import React from "react";
+import MainLayout from "../../components/Layouts/MainLayout/MainLayout";
+import "./Home.css";
+import TextTypo from "../../components/TextTypo/TextTypo";
 
 const Home = () => {
   return (
     <MainLayout>
-      hii
+      <div>
+        <TextTypo text="Dashboard" fontSize="30px" fontWeight="400" />
+      </div>
     </MainLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
